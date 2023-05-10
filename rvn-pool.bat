ef6270cda4859bfc0e2710f992867cf21a0f8fd2
lodio-999 <132812071+lodio-999@users.noreply.github.com>
2023-05-10 23:46:18 +0300

    Update rvn-pool.bat

--- rvn-pool.bat
+++ rvn-pool.bat
@@ -1,2 +1,2 @@
-miner.exe --algo kawpow --server rvn.2miners.com:6060 --user RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.RIG_1
+miner.exe --algo kawpow --server rvn.2miners.com:6060 --user RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.RIG_1 --pass x
 pause
